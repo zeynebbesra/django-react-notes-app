ab79831fa7f0107b35ba141642fc4f4b1d69812b
zeynebbesra <394777@ogr.ktu.edu.tr>
2023-03-07 13:38:41 +0300

    Crud methods created

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('notes/', views.getNotes, name="notes"),
     path('notes/<str:pk>/', views.getNote, name='note'),
+    path('notes/<str:pk>/update/', views.updateNote, name='update-note'),
+    path('notes/<str:pk>/delete/', views.deleteNote, name='delete-note'),
+    path('notes/create/', views.createNote, name='create-note'),
 ]
